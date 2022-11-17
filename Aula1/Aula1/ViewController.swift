d22638bee9a6efb9c3d123f85ab5d9739b03ad0b
ICMMAC03-BEBA <icmmac03-beba@ICMMAC03-BEBAs-Mac-mini.local>
2022-11-17 14:04:42 -0400

    Terminei o projeto para a fazer a aula

---
+++ Aula1/Aula1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aula1
+//
+//  Created by ICMMAC03-BEBA on 17/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
